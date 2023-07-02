9990c51f770859de47b4a25589854c3e22d56962
jihwankim128 <jihwan1535@nate.com>
2023-07-03 06:54:36 +0900

    DataType

---
+++ src/com/javastudy/dataType/CharExam.java
@@ -0,0 +1,16 @@
+package com.javastudy.dataType;
+
+public class CharExam {
+    public static void main(String[] args) {
+        char one = '1';
+        char two = '2';
+        char three = '3';
+        char four = '4';
+        char five = '5';
+
+        int sum = one-'0' + two-'0' + three-'0'
+                + four-'0' + five-'0';
+        System.out.println("'1'+'2'+'3'+'4'+'5'" +
+                " = " + sum);
+    }
+}
---
+++ src/com/javastudy/dataType/FindAPI.java
@@ -0,0 +1,13 @@
+package com.javastudy.dataType;
+
+public class FindAPI {
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+        System.out.println("a = " + a);
+        String s;
+        s = "APPLE";
+        System.out.println("s = " + s);
+        System.out.println(s.length());
+    }
+}
