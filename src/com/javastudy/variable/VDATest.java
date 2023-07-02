d922e03b95e8804cf7b631342706479b05cb7781
jihwankim128 <jihwan1535@nate.com>
2023-07-03 05:52:44 +0900

    variable study

---
+++ src/com/javastudy/variable/VDATest.java
@@ -0,0 +1,20 @@
+package com.javastudy.variable;
+public class VDATest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a;
+        int c = b * 10;
+        System.out.println(a + ", " + b + ", " + c);
+        int sum = 1; System.out.println(sum);
+        sum += 1; System.out.println(sum);
+        sum += 1; System.out.println(sum);
+        sum += 1; System.out.println(sum);
+        sum += 1; System.out.println(sum);
+        int x = 10;
+        int y = 20;
+        int temp = y;
+        y = x;
+        x = temp;
+        System.out.println(x + ", " + y);
+    }
+}
