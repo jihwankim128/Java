2a32789bd8ad1a969f831f28b4623dc8b0c5597f
jihwankim128 <jihwan1535@nate.com>
2023-07-03 05:43:00 +0900

    JavaStudy

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// import lib, class ... etc
+// Class Name
+// JavaSE Program
+public class Main {
+    // method, static var
+    public static void main(String[] args) {
+        // var, function, lib
+        System.out.println("Hello world!");
+    }
+}
