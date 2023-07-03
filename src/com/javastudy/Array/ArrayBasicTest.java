e0345fc6f9e7f9b9c72bbdc317dd844887d6f9b9
jihwankim128 <jihwan1535@nate.com>
2023-07-03 23:45:12 +0900

    Array Study

---
+++ src/com/javastudy/Array/ArrayBasicTest.java
@@ -0,0 +1,9 @@
+package com.javastudy.Array;
+
+public class ArrayBasicTest {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 10; b = 10; c = 10;
+        System.out.println(a + ", " + b + ", " + c);
+    }
+}
---
+++ src/com/javastudy/Array/ArrayMake.java
@@ -0,0 +1,9 @@
+package com.javastudy.Array;
+
+public class ArrayMake {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        String[] s = new String[5];
+        float[] f = new float[5];
+    }
+}
