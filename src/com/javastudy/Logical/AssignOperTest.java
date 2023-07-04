c22e323d23316d095fc4f496b965b07621a5e422
jihwan <jihwan1535@nate.com>
2023-07-04 20:07:33 +0900

    logical

---
+++ src/com/javastudy/Logical/AssignOperTest.java
@@ -0,0 +1,12 @@
+package com.javastudy.Logical;
+
+public class AssignOperTest {
+    public static void main(String[] args) {
+        int a = 1;
+        a++; // a == 2
+        ++a; // a == 3;
+        a = a+1; // a == 4
+        a += 1; // a == 5
+        System.out.println(a);
+    }
+}
