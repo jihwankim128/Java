da63e1084b0a78546be63399303ce68389d082ce
jihwankim128 <jihwan1535@nate.com>
2023-07-07 04:45:43 +0900

    DTO MODEL

---
+++ src/com/javastudy/Programming/Model/CarDTO.java
@@ -0,0 +1,10 @@
+package com.javastudy.Programming.Model;
+
+public class CarDTO {
+    int carSn;
+    String carName;
+    int carPrice;
+    String carOwner;
+    int carYear;
+    String carType;
+}
