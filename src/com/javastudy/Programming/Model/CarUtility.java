f434ef10b886705b67ccd1b213ef65617d3f9a33
jihwankim128 <jihwan1535@nate.com>
2023-07-07 06:46:55 +0900

    Utility Model

---
+++ src/com/javastudy/Programming/Model/CarUtility.java
@@ -0,0 +1,9 @@
+package com.javastudy.Programming.Model;
+
+public class CarUtility {
+    public void carPrint(CarDTO car) {
+        System.out.println(car.carSn + "\t" + car.carName +
+                "\t" + car.carPrice + "\t" + car.carOwner
+                +"\t" + car.carYear + "\t" + car.carType);
+    }
+}
