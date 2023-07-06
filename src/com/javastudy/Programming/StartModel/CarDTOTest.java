98e039be4c220501b6a6102796ed77154dba033a
jihwankim128 <jihwan1535@nate.com>
2023-07-07 04:51:49 +0900

    START MODEL

--- src/com/javastudy/Programming/StartModel/CarDTOTest.java
+++ src/com/javastudy/Programming/StartModel/CarDTOTest.java
@@ -1,7 +1,41 @@
 package com.javastudy.Programming.StartModel;
 
+import com.javastudy.Programming.Model.CarDTO;
+
+import java.util.Scanner;
+
 public class CarDTOTest {
     public static void main(String[] args) {
+        CarDTO car = new CarDTO();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("자동차 번호 : ");
+        car.carSn = scan.nextInt();
+        scan.nextLine();
 
+        System.out.print("자동차 이름 : ");
+        car.carName = scan.nextLine();
+
+        System.out.print("자동차 가격 : ");
+        car.carPrice = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("자동차 소유자 : ");
+        car.carOwner = scan.nextLine();
+
+        System.out.print("자동차 년식 : ");
+        car.carYear = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("자동차 타입 : ");
+        car.carType = scan.nextLine();
+
+        scan.close();
+
+        carInfoPrint(car);
+    }
+    public static void carInfoPrint(CarDTO car){
+        System.out.println(car.carSn + "\t" + car.carName +
+                "\t" + car.carPrice + "\t" + car.carOwner
+                +"\t" + car.carYear + "\t" + car.carType);
     }
 }
