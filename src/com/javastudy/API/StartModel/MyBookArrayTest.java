144ca7aa48a645a9d0713faef84853e0b2d4158c
jihwan <jihwan1535@nate.com>
2023-07-11 21:58:59 +0900

    Using BookArray API

--- src/com/javastudy/API/StartModel/MyBookArrayTest.java
+++ src/com/javastudy/API/StartModel/MyBookArrayTest.java
@@ -1,2 +1,19 @@
-package com.javastudy.API.StartModel;public class MyBookArrayTest {
+package com.javastudy.API.StartModel;
+
+import com.javastudy.API.Model.Book;
+import com.javastudy.API.Utility.BookArray;
+public class MyBookArrayTest {
+    public static void main(String[] args) {
+        BookArray list = new BookArray();
+        list.add(new Book("자바", 15000,
+                "한빛", "홍길동"));
+        list.add(new Book("C++", 17000,
+                "대림", "이길동"));
+        list.add(new Book("Python", 16000,
+                "정보", "나길동"));
+
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
 }
