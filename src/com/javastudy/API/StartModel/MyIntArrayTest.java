ff248f4887ecea1433cbe9ef26a5859183f6d91d
jihwan <jihwan1535@nate.com>
2023-07-11 21:41:10 +0900

    ArrayAPITest

--- src/com/javastudy/API/StartModel/MyIntArrayTest.java
+++ src/com/javastudy/API/StartModel/MyIntArrayTest.java
@@ -1,2 +1,15 @@
-package com.javastudy.API.StartModel;public class MyIntArrayTest {
+package com.javastudy.API.StartModel;
+
+import com.javastudy.API.Utility.IntArray;
+
+public class MyIntArrayTest {
+    public static void main(String[] args) {
+        IntArray list = new IntArray();
+        for (int i = 0; i<6; i++) {
+            list.add(i+1);
+        }
+        for(int i = 0; i<list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
 }
