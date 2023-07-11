efb05e28da3b08afcf6c7c66621c8998931893e4
jihwan <jihwan1535@nate.com>
2023-07-11 21:40:55 +0900

    MAKE ARRAY API

--- src/com/javastudy/API/Utility/IntArray.java
+++ src/com/javastudy/API/Utility/IntArray.java
@@ -1,5 +1,7 @@
 package com.javastudy.API.Utility;
 
+import java.util.Arrays;
+
 public class IntArray {
     private static final int DEFAULT_CAPACITY = 5;
     private int[] elements;
@@ -9,8 +11,17 @@ public class IntArray {
         elements = new int[DEFAULT_CAPACITY];
     }
 
+
     public void add(int element) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
         elements[size++] = element;
+    }
+
+    private void ensureCapacity() {
+        int newCapacity = elements.length*2;
+        elements = Arrays.copyOf(elements, newCapacity);
     }
 
     public int get(int index) {
