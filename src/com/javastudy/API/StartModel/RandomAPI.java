858d922f69c731a761ff49a50d09c17c25fb25a7
jihwan <jihwan1535@nate.com>
2023-07-11 16:38:07 +0900

    External Libraries - JDK

--- src/com/javastudy/API/StartModel/RandomAPI.java
+++ src/com/javastudy/API/StartModel/RandomAPI.java
@@ -1,2 +1,22 @@
-package com.javastudy.API.StartModel;public class RandomAPI {
+package com.javastudy.API.StartModel;
+
+import java.util.Random;
+
+public class RandomAPI {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        boolean[] rands = new boolean[46];
+        int[] arr = new int[6];
+        for(int i = 0; i < arr.length; i++) {
+            int num = rand.nextInt(6)+1;
+            while(rands[num] == true) {
+                num = rand.nextInt(6)+1;
+            }
+            rands[num] = true;
+            arr[i] = num;
+        }
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
