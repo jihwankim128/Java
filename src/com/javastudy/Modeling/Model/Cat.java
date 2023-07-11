c8c6b1f65562772ee2ba01353287f795f4d303e1
jihwankim128 <jihwan1535@nate.com>
2023-07-11 09:29:47 +0900

    Override

--- src/com/javastudy/Modeling/Model/Cat.java
+++ src/com/javastudy/Modeling/Model/Cat.java
@@ -1,6 +1,11 @@
 package com.javastudy.Modeling.Model;
 
 public class Cat extends Animal{
+    @Override
+    public void eat() {
+        System.out.println("고양이처럼 먹다");
+    }
+
     public void night() {
         System.out.println("밤에 빛나");
     }
--- src/com/javastudy/Modeling/Model/Dog.java
+++ src/com/javastudy/Modeling/Model/Dog.java
@@ -1,5 +1,8 @@
 package com.javastudy.Modeling.Model;
 
 public class Dog extends Animal{
-
+    @Override
+    public void eat() {
+        System.out.println("개처럼 먹다");
+    }
 }
