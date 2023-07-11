088f1f1ce729c18d4a72d253c1927418a5500889
jihwan <jihwan1535@nate.com>
2023-07-11 15:33:59 +0900

    Utility Test

--- src/com/javastudy/API/StartModel/MinMaxTest.java
+++ src/com/javastudy/API/StartModel/MinMaxTest.java
@@ -1,2 +1,11 @@
-package com.javastudy.API.StartModel;public class MinMaxTest {
+package com.javastudy.API.StartModel;
+
+import com.javastudy.API.Utility.MinMaxFinder;
+
+public class MinMaxTest {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 9, 1, 7};
+        System.out.println("Max = " + MinMaxFinder.findMax(arr));
+        System.out.println("Min = " + MinMaxFinder.findMin(arr));
+    }
 }
