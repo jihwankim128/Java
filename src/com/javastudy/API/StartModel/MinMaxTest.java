2eafcf5ba70f712856a28608ba9cb9de4643ff4b
jihwan <jihwan1535@nate.com>
2023-07-11 15:33:40 +0900

    MinMaxAPI

---
+++ src/com/javastudy/API/StartModel/MinMaxTest.java
@@ -0,0 +1,2 @@
+package com.javastudy.API.StartModel;public class MinMaxTest {
+}
---
+++ src/com/javastudy/API/Utility/MinMaxFinder.java
@@ -0,0 +1,22 @@
+package com.javastudy.API.Utility;
+
+public class MinMaxFinder {
+    public static int findMin(int[] a) {
+        int min = a[0];
+        for(int i = 1; i<a.length; i++) {
+            if(a[i] < min) {
+                min = a[i];
+            }
+        }
+        return min;
+    }
+    public static int findMax(int[] a) {
+        int max = a[0];
+        for(int i = 1; i<a.length; i++) {
+            if(a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+}
