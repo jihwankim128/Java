beceffdf720511660cdef627e1ef42dfdd04a1b2
jihwan <jihwan1535@nate.com>
2023-07-11 16:38:01 +0900

    External Libraries - Gson

--- src/com/javastudy/API/StartModel/GsonFromAPI.java
+++ src/com/javastudy/API/StartModel/GsonFromAPI.java
@@ -1,2 +1,13 @@
-package com.javastudy.API.StartModel;public class GsonFromAPI {
+package com.javastudy.API.StartModel;
+
+import com.google.gson.Gson;
+import com.javastudy.API.Model.Person;
+
+public class GsonFromAPI {
+    public static void main(String[] args) {
+        String json = "{\"name\":\"jihwan\",\"age\":24}";
+        Gson gson = new Gson();
+        Person p = gson.fromJson(json, Person.class);
+        System.out.println(p);
+    }
 }
--- src/com/javastudy/API/StartModel/GsonToAPI.java
+++ src/com/javastudy/API/StartModel/GsonToAPI.java
@@ -1,2 +1,13 @@
-package com.javastudy.API.StartModel;public class GsonToAPI {
+package com.javastudy.API.StartModel;
+
+import com.google.gson.Gson;
+import com.javastudy.API.Model.Person;
+
+public class GsonToAPI {
+    public static void main(String[] args) {
+        Person person = new Person("jihwan", 24);
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
 }
