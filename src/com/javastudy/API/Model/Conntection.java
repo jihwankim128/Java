45ac21e8adb89c01bb75a1f4b9d2bfe32a2958c5
jihwankim128 <jihwan1535@nate.com>
2023-07-16 06:53:32 +0900

    DB Test

---
+++ src/com/javastudy/API/Model/Conntection.java
@@ -0,0 +1,7 @@
+package com.javastudy.API.Model;
+
+public interface Conntection {
+    public void getConnection(String url,
+                              String username,
+                              String password);
+}
