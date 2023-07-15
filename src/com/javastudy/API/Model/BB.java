815fc7a1aee83ecc199d81c16373afb0999082ed
jihwankim128 <jihwan1535@nate.com>
2023-07-16 06:40:35 +0900

    secret model

---
+++ src/com/javastudy/API/Model/BB.java
@@ -0,0 +1,18 @@
+package com.javastudy.API.Model;
+
+public class BB implements CC{
+    @Override
+    public void x() {
+        System.out.println("X 실행");
+    }
+
+    @Override
+    public void y() {
+        System.out.println("y 실행");
+    }
+
+    @Override
+    public void z() {
+        System.out.println("z 실행");
+    }
+}
