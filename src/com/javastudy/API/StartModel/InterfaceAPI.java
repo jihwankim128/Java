2ae60f7031262c0393800c97633bc0275cf7f9bb
jihwankim128 <jihwan1535@nate.com>
2023-07-16 06:42:29 +0900

    Interface Base Programming

---
+++ src/com/javastudy/API/StartModel/InterfaceAPI.java
@@ -0,0 +1,13 @@
+package com.javastudy.API.StartModel;
+
+import com.javastudy.API.Model.BB;
+import com.javastudy.API.Model.CC;
+
+public class InterfaceAPI {
+    public static void main(String[] args) {
+        CC c = new BB();
+        c.x();
+        c.y();
+        c.z();
+    }
+}
