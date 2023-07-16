f74bd4dccd870c7427621bf02abded31c3624532
jihwankim128 <jihwan1535@nate.com>
2023-07-16 12:08:17 +0900

    HashSet

--- src/com/javastudy/Collection/StartModel/HashSetExample.java
+++ src/com/javastudy/Collection/StartModel/HashSetExample.java
@@ -1,2 +1,37 @@
-package com.javastudy.Collection.StartModel;public class HashSetExample {
+package com.javastudy.Collection.StartModel;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Apple");
+        set.add("Banana");
+        set.add("Cherry");
+        set.add("Banana");
+
+        System.out.println(set.size());
+
+        for (String s : set) {
+            System.out.println(s);
+        }
+
+        set.remove("Banana");
+        System.out.println("\nRemove\n");
+
+        for (String s : set) {
+            System.out.println(s);
+        }
+
+        //삭제할 요소가 없을 경우 false
+        System.out.println(set.remove("Banana"));
+
+        //contains로 먼저 찾고 삭제하는 듯
+        System.out.println(set.contains("Banana"));
+
+        set.clear();
+
+        System.out.println(set.isEmpty());
+    }
 }
