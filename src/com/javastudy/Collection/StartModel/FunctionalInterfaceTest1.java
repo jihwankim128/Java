a9bf1ea100255b6440a5618c94892ee5669d482e
jihwankim128 <jihwan1535@nate.com>
2023-07-17 00:32:45 +0900

    FunctionalInterfaceTest1

--- src/com/javastudy/Collection/StartModel/FunctionalInterfaceTest1.java
+++ src/com/javastudy/Collection/StartModel/FunctionalInterfaceTest1.java
@@ -2,9 +2,9 @@ package com.javastudy.Collection.StartModel;
 
 import com.javastudy.Collection.Model.MathOperation;
 
-public class FunctionalInterfaceTest implements MathOperation {
+public class FunctionalInterfaceTest1 implements MathOperation {
     public static void main(String[] args) {
-        MathOperation m = new FunctionalInterfaceTest();
+        MathOperation m = new FunctionalInterfaceTest1();
         int result = m.operation(10, 20);
         System.out.println(result);
     }
