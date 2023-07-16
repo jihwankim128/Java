d3b95d64246d8aa2b76ee1aa722f89094c2ad1b5
jihwankim128 <jihwan1535@nate.com>
2023-07-17 00:31:03 +0900

    Implements FunctionalInterface

---
+++ src/com/javastudy/Collection/Model/MathOperationImpl.java
@@ -0,0 +1,8 @@
+package com.javastudy.Collection.Model;
+
+public class MathOperationImpl implements MathOperation{
+    @Override
+    public int operation(int x, int y) {
+        return x+y;
+    }
+}
