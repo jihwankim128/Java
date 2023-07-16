4b22ab38fb3ee17d0f2348a9ef012dd2b142ba1b
jihwankim128 <jihwan1535@nate.com>
2023-07-16 10:31:57 +0900

    Add int into Array List

--- src/com/javastudy/Collection/StartModel/CollectionBasic.java
+++ src/com/javastudy/Collection/StartModel/CollectionBasic.java
@@ -1,7 +1,15 @@
 package com.javastudy.Collection.StartModel;
 
+import java.util.ArrayList;
+
 public class CollectionBasic {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 1; i<=5; i++) {
+            list.add(i*10);
+        }
+        for (int data : list) {
+            System.out.println(data);
+        }
     }
 }
