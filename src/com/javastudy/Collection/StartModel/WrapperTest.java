f938762f38bb8bbe1ad488466feeea42893c34cc
jihwankim128 <jihwan1535@nate.com>
2023-07-16 10:19:36 +0900

    auto Boxing, Unboxing

---
+++ src/com/javastudy/Collection/StartModel/WrapperTest.java
@@ -0,0 +1,16 @@
+package com.javastudy.Collection.StartModel;
+
+public class WrapperTest {
+    public static void main(String[] args) {
+        //Boxing
+        Integer a = new Integer(10);
+        //UnBoxing
+        System.out.println(a.intValue());
+
+        //auto Boxing
+        Integer aa = 10;
+        //auto UnBoxing
+        System.out.println(aa);
+
+    }
+}
