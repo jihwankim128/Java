548444503c641cd6763676ab709ca55d3cb269ef
jihwankim128 <jihwan1535@nate.com>
2023-07-16 10:42:27 +0900

    List<string> Test

--- src/com/javastudy/Collection/StartModel/ListExample.java
+++ src/com/javastudy/Collection/StartModel/ListExample.java
@@ -1,2 +1,26 @@
-package com.javastudy.Collection.StartModel;public class ListExample {
+package com.javastudy.Collection.StartModel;
+
+import java.util.ArrayList;
+
+public class ListExample {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("banana");
+        list.add("cherry");
+        list.add("banana");
+
+        for(int i = 0; i<list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        list.remove(0);
+        System.out.println("삭제");
+
+        list.set(1, "orange");
+
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
 }
