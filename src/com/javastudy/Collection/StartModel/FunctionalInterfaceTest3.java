731188f80842267d2885f91c9f97cd06e653b3e4
jihwankim128 <jihwan1535@nate.com>
2023-07-17 00:36:43 +0900

    FunctionalInterfaceTest3

--- src/com/javastudy/Collection/StartModel/FunctionalInterfaceTest3.java
+++ src/com/javastudy/Collection/StartModel/FunctionalInterfaceTest3.java
@@ -1,4 +1,17 @@
 package com.javastudy.Collection.StartModel;
 
+import com.javastudy.Collection.Model.MathOperation;
+
 public class FunctionalInterfaceTest3 {
+    public static void main(String[] args) {
+        //익명 내부클래스
+        MathOperation mo = new MathOperation() {
+            @Override
+            public int operation(int x, int y) {
+                return x+y;
+            }
+        };
+
+        System.out.println(mo.operation(10, 20));
+    }
 }
