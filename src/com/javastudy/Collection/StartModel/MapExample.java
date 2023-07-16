3d7dedc4033d34cf960bd4c06dc7522992b07f75
jihwankim128 <jihwan1535@nate.com>
2023-07-16 13:59:11 +0900

    Hash Map

---
+++ src/com/javastudy/Collection/StartModel/MapExample.java
@@ -0,0 +1,28 @@
+package com.javastudy.Collection.StartModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapExample {
+    public static void main(String[] args) {
+        Map<String, Integer> stdScores = new HashMap<>();
+        stdScores.put("Kim", 95);
+        stdScores.put("Lee", 85);
+        stdScores.put("Park", 90);
+        stdScores.put("Choi", 80);
+
+        System.out.println("Kim's score : " + stdScores.get("Kim"));
+        System.out.println("Lee's score : " + stdScores.get("Lee"));
+
+        stdScores.put("Park", 92); // 수정 됨
+        System.out.println("Park's score : " + stdScores.get("Park"));
+
+        stdScores.remove("Choi");
+        System.out.println("Choi's score : " + stdScores.get("Choi"));
+
+        for(Map.Entry<String, Integer> entry : stdScores.entrySet()){
+            System.out.println(entry.getKey() + "'s Score = " + entry.getValue());
+        }
+    }
+}
