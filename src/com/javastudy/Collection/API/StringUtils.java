4a40e3b5c0cfbdbb1d52d0e04c4ad494c4e3068a
jihwankim128 <jihwan1535@nate.com>
2023-07-17 18:39:26 +0900

    String reverse ㅕ냐using StringReverse API

--- src/com/javastudy/Collection/API/StringUtils.java
+++ src/com/javastudy/Collection/API/StringUtils.java
@@ -2,6 +2,7 @@ package com.javastudy.Collection.API;
 
 public class StringUtils {
     public String reverse(String s) {
+        //문자열 변경을 할 때 StringBuffer
         return new StringBuffer(s).reverse().toString();
     }
 }
